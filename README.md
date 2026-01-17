a3d6201d58642ef9dc23b50378c3bc4e57d60d37
Willow <will7hughes@gmail.com>
2026-01-16 23:01:06 -0600

    Initial commit from basic setup

---
+++ README.md
@@ -0,0 +1,4 @@
+# Hytale Plugin Template
+
+A template for Hytale java plugins. Created by Up, and slightly modified by Kaupenjoe. 
+
---
+++ src/main/java/com/willowaway/justafly/FlyPlugin.java
@@ -0,0 +1,19 @@
+package com.willowaway.justafly;
+
+import com.hypixel.hytale.logger.HytaleLogger;
+import com.hypixel.hytale.server.core.plugin.JavaPlugin;
+import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
+
+public class FlyPlugin extends JavaPlugin {
+    private static final HytaleLogger LOGGER = HytaleLogger.forEnclosingClass();
+
+    public FlyPlugin(JavaPluginInit init) {
+        super(init);
+        LOGGER.atInfo().log("Hello from %s version %s", this.getName(), this.getManifest().getVersion().toString());
+    }
+
+    @Override
+    protected void setup() {
+        this.getCommandRegistry().registerCommand(new ExampleCommand(this.getName(), this.getManifest().getVersion().toString()));
+    }
+}
